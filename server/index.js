e206169068283de4647b6c8bf6b362b1c3d49df4
Handsell <90242087+Bquanganh@users.noreply.github.com>
2022-07-07 17:17:32 +0700

    create client and server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import posts from './routers/posts.js'
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+const URI = 'mongodb+srv://quanganh04012000:Nhoxanh2233@cluster0.giulr.mongodb.net/Blog'
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true,limit: '30mb'}));
+app.use(cors());
+
+
+app.use('/posts',posts);
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then (()=>{
+    console.log('Connected to DB');
+}).catch(err => {
+    console.log('err',err)
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
