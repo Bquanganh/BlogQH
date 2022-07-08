9955278aa000950579fee2d4a91dee309a7cf237
Handsell <90242087+Bquanganh@users.noreply.github.com>
2022-07-08 18:08:52 +0700

    update client to call api from server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import posts from './routers/posts.js'
 import mongoose from 'mongoose';
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 5000;
 
 const URI = 'mongodb+srv://quanganh04012000:Nhoxanh2233@cluster0.giulr.mongodb.net/Blog'
 
