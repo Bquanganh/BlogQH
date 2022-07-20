7253d5925b8ddea187e7d2905c7abdd902359c78
Handsell <90242087+Bquanganh@users.noreply.github.com>
2022-07-20 11:47:22 +0700

    add client

---
+++ client/src/redux/reducers/posts.js
@@ -0,0 +1,24 @@
+import {INIT_STATE} from '../../constant'
+import { getPosts, getType } from '../actions'
+
+export default function postsReducer(state= INIT_STATE.posts, action) {
+    switch (action.type) {
+        case getType(getPosts.getPostsRequest):
+            return {
+                ...state,
+                isLoading:true,
+            }
+        case getType(getPosts.getPostsSuccess):
+            return {
+                ...state,
+                isLoading:false
+            }
+        case getType(getPosts.getPostsFailure):
+            return {
+                ...state,
+                isLoading:false
+            }
+        default:
+            return state;
+    }
+}
